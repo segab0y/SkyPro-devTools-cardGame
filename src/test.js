8eefa4247dd11b4f2d3148d0194d92ba16ea4e4f
en0t <perm4.lfw@gmail.com>
2022-09-28 12:10:07 +0500

    test lint commit

---
+++ src/test.js
@@ -0,0 +1,11 @@
+const add = (a, b) => {
+   // const das = ''
+   if (b === '1') {
+      return
+   }
+   return a + b
+}
+
+const abcCba = document.querySelector('.body')
+
+console.log(abcCba + add + '123')
